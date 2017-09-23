d4b0f41655fec075fdfad5d2c2897d9b2a70882f
Justinas Ulevicius <justinas.ulevicius@gmail.com>
2017-09-23 17:32:30 -0400

    Initial commit

---
+++ onwrapper/open_notify.py
@@ -0,0 +1,41 @@
+import requests
+#to do: have exceptions if return code not 200 or message not success
+class OpenNotify(object):
+	"""
+	A Python wrapper for the Open Notify API.
+	http://http://open-notify.org/
+	"""
+
+	def astronauts(self):
+		"""
+		Returns the number of people currently in spcace.
+		Returns a list of all the astronauts and their spacecraft.
+		"""
+		endpoint = "http://api.open-notify.org/astros.json"
+		response = requests.get(endpoint)
+
+		return (response.json()["number"],
+			    response.json()["people"])
+
+	def location(self):
+		"""
+		Returns the current longitude and latitude of the ISS.
+		"""
+		endpoint = "http://api.open-notify.org/iss-now.json"
+		response = requests.get(endpoint)
+		print(response.json())
+
+		return (response.json()["iss_position"]["longitude"],
+			    response.json()["iss_position"]["latitude"])
+
+	def pass_prediction(self, latitude, longitude, n):
+		"""
+		Returns the pass times/dates for given coordinates.
+		"""
+		endpoint = "http://api.open-notify.org/iss-pass.json?lat={}&lon={}&alt=20&n={}".format(latitude, longitude, n)
+		response = requests.get(endpoint)
+
+		return response.json()
+
+cl = OpenNotify()
+print(cl.astronauts())
---
+++ tests/test_open_notify.py
@@ -0,0 +1,9 @@
+from onwrapper import OpenNotify
+
+def test_astronauts():
+	"""
+	Tests the functionality of the astronauts API call.
+	"""
+	open_notify = OpenNotify()
+	num_of_astronauts, astronauts = open_notify.astronauts()
+	assert type(num) == int
